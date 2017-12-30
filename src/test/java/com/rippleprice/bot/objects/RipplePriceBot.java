61c14f6f9459efa6cccb605853a22ac2428a3adc
Anton Fifindik <fifindik.av@gmail.com>
2017-12-30 20:28:33 +0200

    refactoring using CoinMarketCap JSON API in /price command

--- src/test/java/com/rippleprice/bot/objects/RipplePriceBot.java
+++ src/test/java/com/rippleprice/bot/objects/RipplePriceBot.java
@@ -3,8 +3,10 @@ package com.rippleprice.bot.objects;
 import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -33,6 +35,7 @@ public class RipplePriceBot extends TelegramLongPollingBot {
 
 	public void onUpdateReceived(Update arg0) {
 
+		Locale.setDefault(Locale.ENGLISH);
 		Message msg = arg0.getMessage();
 		StringBuilder result = new StringBuilder();
 		Document doc;
@@ -43,14 +46,15 @@ public class RipplePriceBot extends TelegramLongPollingBot {
 				sendMsg(msg, "/price - current price of a Ripple\n/markets - Markets price\n/exmo - Exmo.com price\n/history - Weekly history\n/top10 - Top 10 cryptocurrency market capitalizations");
 			if (msg.getText().equals("/price") || msg.getText().equals("/price@RipplePrice_bot")) {
 				try {
-					doc = Jsoup.connect("https://coinmarketcap.com/currencies/ripple/").get();
-					String[] marketCap = doc.select(".coin-summary-item-detail.details-text-medium>span").text().split(" ");
-					String[] priceChange = doc.select(".text-large2").text().split(" ");
-					sendMsg(msg,
-							"Current price: *$" + doc.select("span#quote_price").text().replace(" USD", "") + "*\n" + "Price change: " + priceChange[1].replace("(", "").replace(")", "") + "\n" + "———————————" + "\n" + "Market Cap: " + "\n"
-									+ marketCap[0] + " " + marketCap[1] + "\n" + marketCap[2] + " " + marketCap[3] + "\n" + "———————————" + "\n" + "Volume (24h): " + "\n" + marketCap[4] + " " + marketCap[5] + "\n" + marketCap[6] + " " + marketCap[7]
-									+ "\n\n" + "information by coinmarketcap.com");
+					jsonArray = JsonReader.readJsonFromUrl("https://api.coinmarketcap.com/v1/ticker/ripple/");
+					JSONObject jsonObject = jsonArray.getJSONObject(0);
 
+					result.append(String.format("Current price: *%s*\nChange(24h): %s%%\n———————————\nMarket cap: \n%s\nRank: %s\n———————————\nVolume(24h):\n%s\nСhange(7d): %s%%", new DecimalFormat("$#0.00").format(jsonObject.getDouble("price_usd")),
+							jsonObject.getString("percent_change_24h"), new DecimalFormat("$###,###.###").format(jsonObject.getDouble("market_cap_usd")), jsonObject.getString("rank"),
+							new DecimalFormat("$###,###.###").format(jsonObject.getDouble("24h_volume_usd")), jsonObject.getString("percent_change_7d")));
+
+					result.append("\n\ninformation by coinmarketcap.com");
+					sendMsg(msg, result.toString());
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
@@ -116,9 +120,9 @@ public class RipplePriceBot extends TelegramLongPollingBot {
 					jsonArray = JsonReader.readJsonFromUrl("https://api.coinmarketcap.com/v1/ticker/?limit=10");
 					for (int i = 0; i < jsonArray.length(); i++) {
 
-						result.append(String.format("%s. *%s* (%s)\nPrice: *$%s*\nMarket Cap:\n$%s\nChange (24h):\n%s%%", jsonArray.getJSONObject(i).get("rank"), jsonArray.getJSONObject(i).get("name"), jsonArray.getJSONObject(i).get("symbol"),
-								new DecimalFormat("#0.00").format(new Double(jsonArray.getJSONObject(i).get("price_usd").toString())).replace(',', '.'),
-								new DecimalFormat("###,###.###").format(new Double(jsonArray.getJSONObject(i).get("market_cap_usd").toString())), jsonArray.getJSONObject(i).get("percent_change_24h")));
+						result.append(String.format("%s. *%s* (%s)\nPrice: *$%s*\nMarket Cap:\n%s\nChange(24h):\n%s%%", jsonArray.getJSONObject(i).get("rank"), jsonArray.getJSONObject(i).get("name"), jsonArray.getJSONObject(i).get("symbol"),
+								new DecimalFormat("#0.00").format(jsonArray.getJSONObject(i).getDouble("price_usd")).replace(',', '.'), new DecimalFormat("$###,###.###").format(jsonArray.getJSONObject(i).getDouble("market_cap_usd")),
+								jsonArray.getJSONObject(i).get("percent_change_24h")));
 						if (i < jsonArray.length() - 1)
 							result.append("\n———————————\n");
 					}
@@ -143,7 +147,7 @@ public class RipplePriceBot extends TelegramLongPollingBot {
 						if (data[1].equals("Ripple") && (data[2].equals("XRP/USD") || data[2].equals("XRP/BTC"))) {
 							int resultLength = result.length();
 
-							result.append("Pair: " + data[2] + "\nPrice: *" + data[4] + "*\nVolume(24h):\n" + data[3] + "\nVolume(%):\n" + data[5]);
+							result.append("Pair: " + data[2] + "\nPrice: *" + data[4] + "*\nVolume(24h):\n" + data[3] + "\n" + data[5]);
 							if (resultLength == 0)
 								result.append("\n———————————\n");
 							else
